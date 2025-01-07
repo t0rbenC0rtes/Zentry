9f4e5727e8386203a4adb1fd5f9d58713a8030b4
Antoine Lafontaine <antoinelaf@gmail.com>
2025-01-07 16:17:07 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import React from "react";
+import About from "./components/About.jsx";
+import Hero from "./components/Hero.jsx";
+
+const App = () => {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+
+      <About />
+    </main>
+  );
+};
+
+export default App;
